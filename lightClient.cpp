8382fa7afda86ef4a09d92e511480a9ab63a0d1a
vc-oreange <397791749@qq.com>
2021-01-21 23:13:26 +0800

    add SELECT for I/O multiplexing

--- lightClient.cpp
+++ lightClient.cpp
@@ -10,13 +10,19 @@
 #include <unistd.h>
 #include <iostream>
 #include <string>
+#include <sys/select.h>
+
+
 const int port = 8888;
 #define RCV_BUFF_SIZE 1024
 #define SND_BUFF_SIZE 520
 
 int main(int argc, char *argv[])
 {
-
+    std::string name;
+    std::cout<<"输入名称: ";
+    std::cin>>name;
+    std::cout<<"欢迎进入聊天室"<<std::endl;
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = PF_INET;
@@ -32,16 +38,42 @@ int main(int argc, char *argv[])
         printf("Can not connect to %s",inet_ntoa(server_address.sin_addr));
     }
 
-    std::string sendMessage = "hello Server";
-    ret = send(sock, sendMessage.data(), sizeof(sendMessage), 0);
+    std::string sendMessage = name+" Connect to Server\n";
+    ret = send(sock, sendMessage.data(), sizeof(sendMessage)+1, 0);
     assert(ret!=-1);
 
-    
-    while(std::cin>>sendMessage){
-        ret = send(sock, sendMessage.data(), sizeof(sendMessage), 0);
-        assert(ret!=-1);
-        if(sendMessage.compare("close")==0)
-            break;
+    int keyInput=open("/dev/tty",O_RDONLY|O_NONBLOCK);
+    assert(keyInput>0);
+
+    int max_fd=keyInput>sock?keyInput:sock;
+    fd_set read_fd;
+    char sendMsg[SND_BUFF_SIZE];
+    strcpy(sendMsg,name.data());
+    strcat(sendMsg," : ");
+    int nameLenth=strlen(sendMsg);
+    char recvMsg[RCV_BUFF_SIZE];
+    char nameRcv[20];
+    while(1){
+        FD_ZERO(&read_fd);
+        FD_SET(keyInput,&read_fd);
+        FD_SET(sock,&read_fd);
+        select(max_fd+1,&read_fd,NULL,NULL,NULL);
+        if(FD_ISSET(keyInput,&read_fd)){
+            memset(sendMsg+nameLenth,0,SND_BUFF_SIZE-nameLenth);
+            ret = read(keyInput,sendMsg+nameLenth,SND_BUFF_SIZE);
+            send(sock,sendMsg,strlen(sendMsg),0);
+            printf("\r");
+        }
+        if(FD_ISSET(sock,&read_fd)){
+            ret = recv(sock,&recvMsg,RCV_BUFF_SIZE,0);
+            if(ret ==0 ){
+                std::cout<<"服务端关闭"<<std::endl;
+                break;
+            }
+            recvMsg[ret]='\0';
+            if(strncmp(name.data(),recvMsg,name.size())!=0)
+                std::cout<<recvMsg;
+        }
     }
     close(sock);
     return 0;
--- lightServer.cpp
+++ lightServer.cpp
@@ -10,22 +10,38 @@
 #include <unistd.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sys/select.h>
+#include <set>
+#include <arpa/inet.h>
 
 const int port = 8888;
 #define RCV_BUFF_SIZE 1024
 #define SND_BUFF_SIZE 520
+#define LIMIT_FD_NUM 1024
 
-int HandleClient(int fd_client)
+enum clientCmd
+{
+    DEFAULT,
+    CLOSE
+};
+
+int HandleClient(int fd_client, char *Msg)
 {
     char request[RCV_BUFF_SIZE];
 
-    while(recv(fd_client, request, RCV_BUFF_SIZE, 0)){
-        if(strcmp(request,"close")==0)
-            break;
-        request[strlen(request) + 1] = '\0';
-        printf("%s\nsuccesseful\n", request);
+    int ret = recv(fd_client, request, RCV_BUFF_SIZE, 0);
+    if (ret == 0)
+    {
+        return clientCmd::CLOSE;
     }
-    return 1;
+    if (ret != -1)
+    {
+        request[ret] = '\0';
+        printf("%s\nsuccesseful message form clinet %d\n", request, fd_client);
+    }
+    strcat(Msg, request);
+    return clientCmd::DEFAULT;
 }
 
 int main(int argc, char *argv[])
@@ -46,21 +62,84 @@ int main(int argc, char *argv[])
 
     ret = listen(sock, 1);
     assert(ret != -1);
+
+    std::set<int> rd_fds;
+    rd_fds.insert(sock);
+    std::vector<int> client_closed; //临时数组，记录要把哪些关闭的CLIENT_FD从RD_FDS上移除
+
+    fd_set rd_fd_set;
+    FD_ZERO(&rd_fd_set);
+    FD_SET(sock, &rd_fd_set);
+
+    struct sockaddr_in client;
+
+    char recvMsg[RCV_BUFF_SIZE];
+    socklen_t client_addrlength = sizeof(client);
+
     while (1)
     {
-        struct sockaddr_in client;
-        socklen_t client_addrlength = sizeof(client);
+        select(*rd_fds.rbegin() + 1, &rd_fd_set, NULL, NULL, NULL);
+        if (FD_ISSET(sock, &rd_fd_set))
+        {
 
-        int connfd = accept(sock, (struct sockaddr *)&client, &client_addrlength);
-        if (connfd < 0)
-        {
-            printf("建立连接失败");
+            int con_fd = accept(sock, (struct sockaddr *)&client, &client_addrlength);
+            if (con_fd < 0)
+            {
+                printf("建立连接失败");
+            }
+            else
+            {
+                std::cout << "成功连接" << std::endl;
+                rd_fds.insert(con_fd);
+            }
         }
-        else
+
+        for (auto i : rd_fds)
         {
-            HandleClient(connfd);
-            close(connfd);
-            std::cout<<"已断开连接"<<std::endl<<std::endl;
+            if (i == sock)
+                continue;
+            if (FD_ISSET(i, &rd_fd_set))
+            {
+                int cmd = HandleClient(i, recvMsg);
+                if (cmd == -1)
+                {
+                    getpeername(i, (struct sockaddr *)&client, &client_addrlength);
+                    std::cout << "发生错误，客户端FD为" << i << std::endl
+                              << "对方IP为" << inet_ntoa(client.sin_addr) << std::endl;
+                    client_closed.push_back(i);
+                    continue;
+                }
+                switch (cmd)
+                {
+                case clientCmd::CLOSE:
+                    getpeername(i, (struct sockaddr *)&client, &client_addrlength);
+                    std::cout << "客户端:" << i << "断开连接" << std::endl;
+                    client_closed.push_back(i);
+                    close(i);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        for (auto fd : client_closed)
+        {
+            rd_fds.erase(fd);
+        }
+        client_closed.clear();
+
+        for (auto fd : rd_fds)
+        {
+            if (fd != sock)
+            {
+                send(fd, recvMsg, strlen(recvMsg), 0);
+            }
+        }
+        memset(recvMsg, 0, RCV_BUFF_SIZE);
+        FD_ZERO(&rd_fd_set);
+        for (auto i : rd_fds)
+        {
+            FD_SET(i, &rd_fd_set);
         }
     }
     return 0;
